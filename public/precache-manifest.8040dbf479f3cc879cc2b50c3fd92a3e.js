c9087ff06f59ebd3d2854097d08e0e21a156795a
Marwen Aouiti <marwen.aouiti@outlook.fr>
2020-07-19 17:33:09 +0100

    added frontend

---
+++ public/precache-manifest.8040dbf479f3cc879cc2b50c3fd92a3e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b6aa0f7086cf979122aaa83d09b4256",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea2f216bda8fe8ae8293",
+    "url": "/static/css/0.c95de083.chunk.css"
+  },
+  {
+    "revision": "56c603161ef886589a17",
+    "url": "/static/css/4.061f73cd.chunk.css"
+  },
+  {
+    "revision": "09946ccaea84d72ad9ac",
+    "url": "/static/css/5.ccbff8d1.chunk.css"
+  },
+  {
+    "revision": "732b33de1b9516365a02",
+    "url": "/static/css/6.02b58185.chunk.css"
+  },
+  {
+    "revision": "ec133464458f96215829",
+    "url": "/static/css/7.f6f83b8f.chunk.css"
+  },
+  {
+    "revision": "4e5bb17865e6dd01e912",
+    "url": "/static/css/8.15138960.chunk.css"
+  },
+  {
+    "revision": "bb4857a28e6cd1cd0f55",
+    "url": "/static/css/main.46223655.chunk.css"
+  },
+  {
+    "revision": "ea2f216bda8fe8ae8293",
+    "url": "/static/js/0.e810627b.chunk.js"
+  },
+  {
+    "revision": "70e6934a99dbf04843e0",
+    "url": "/static/js/3.90a6c3e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/3.90a6c3e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56c603161ef886589a17",
+    "url": "/static/js/4.43e03f77.chunk.js"
+  },
+  {
+    "revision": "09946ccaea84d72ad9ac",
+    "url": "/static/js/5.e6854329.chunk.js"
+  },
+  {
+    "revision": "732b33de1b9516365a02",
+    "url": "/static/js/6.7ea50830.chunk.js"
+  },
+  {
+    "revision": "ec133464458f96215829",
+    "url": "/static/js/7.07f4bec5.chunk.js"
+  },
+  {
+    "revision": "4e5bb17865e6dd01e912",
+    "url": "/static/js/8.450da584.chunk.js"
+  },
+  {
+    "revision": "bb4857a28e6cd1cd0f55",
+    "url": "/static/js/main.27cdac8c.chunk.js"
+  },
+  {
+    "revision": "85d3455c2e59c3295069",
+    "url": "/static/js/runtime-main.89cba083.js"
+  }
+]);
