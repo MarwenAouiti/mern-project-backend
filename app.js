f17d912d4227908b296544cc0f5ccaa38ce6e55c
Marwen Aouiti <marwen.aouiti@outlook.fr>
2020-07-19 16:31:30 +0100

    added PORT env

--- app.js
+++ app.js
@@ -48,5 +48,5 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-guslo.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
   )
-  .then(() => app.listen(5000))
+  .then(() => app.listen(process.env.PORT || 5000))
   .catch((err) => console.log(err));
